0b4bedbff38f42695b2175145955da6208b1c281
Deepanshu Kohli <deepanshukohli47@gmail.com>
2023-09-28 17:55:13 +0530

    Redux setup done with folder structure

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,43 +1,60 @@
 import React, { useState } from 'react'
 import image from '../Images/expensebg.jpg';
+import axios from "axios";
+import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Login = () => {
-    const [error, setError] = useState(false);
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const login = () => {
+        console.log(email, password)
+        axios.post('http://127.0.0.1:8000/api/login', {
+            email: email,
+            password: password
+        })
+            .then(response => {
+                console.log(response.data);
+                console.log(response.data.data);
+                toast.success(response.data.message);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
     return (
         <>
 
-            <div style={{ "height": "100vh", backgroundImage: `url(${image})`,backgroundRepeat:"no-repeat",backgroundSize:"cover",backgroundPosition:"center",position:"relative" }} >
-            <div style={{position: "absolute",top: 0,left: 0,right: 0,bottom: 0,backdropFilter: 'blur(.5rem)',}} className='flex-column w-100 align-items-center justify-content-center d-flex'>
-                
-                <div className="container border rounded p-5 flex-column align-items-center justify-content-around d-flex" style={{ "background-color": "white", "height": "auto","width":"auto", }}>
-                <h1 style={{ "color": "blue", "marginBottom": "1rem" }}>Expense Manager</h1>
-                    <div className="row mb-3">
-                        <div className="col">
-                            <h2>Login</h2>
+            <div style={{ "height": "100vh", backgroundImage: `url(${image})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center", position: "relative" }} >
+                <div style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0, backdropFilter: 'blur(1rem)', }} className='flex-column w-100 align-items-center justify-content-center d-flex'>
+
+                    <div className="container border rounded p-5 flex-column align-items-center justify-content-around d-flex" style={{ backgroundColor: "white", "height": "auto", "width": "auto", }}>
+                        <h1 style={{ "color": "blue", "marginBottom": "1rem" }}>Expense Manager</h1>
+                        <div className="row mb-3">
+                            <div className="col">
+                                <h2>Login</h2>
+                            </div>
                         </div>
-                    </div>
-                    <div className="row my-2">
-                        <div className="col">
-                            <input className='px-2 rounded' type="text" name="email" id="" required placeholder="Email" />
+                        <div className="row my-2">
+                            <div className="col">
+                                <input className='px-2 rounded' type="email" name="email" id="" required placeholder="Email" value={email} onChange={(e) => { setEmail(e.target.value) }} />
+                            </div>
                         </div>
-                    </div>
-                    <div className="row my-2">
-                        <div className="col">
-                            <input className='px-2 rounded' type="password" name="password" id="" required placeholder="Password" />
+                        <div className="row my-2">
+                            <div className="col">
+                                <input className='px-2 rounded' type="password" name="password" id="" required placeholder="Password" value={password} onChange={(e) => { setPassword(e.target.value) }} />
+                            </div>
                         </div>
-                    </div>
-                    {
-                        error ? <div className="row my-2"> <div className="col">
-                            <p style={{ "color": "red" }}>Please enter Valid Credentials</p>
-                        </div></div> : <></>
-                    }
-                    <div className="row my-4">
-                        <div className="col">
-                            <button className='btn btn-success btn-sm'>Login</button>
+                        <div className="row my-4">
+                            <div className="col">
+                                <button onClick={login} className='btn btn-success btn-sm'>Login</button>
+                            </div>
                         </div>
-                    </div>
-                </div >
+                    </div >
                 </div>
             </div>
+            <ToastContainer autoClose={800} />
         </>
     )
 }
